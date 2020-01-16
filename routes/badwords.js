374d1eac182d2a4da910a54e644b3a0501f58448
katherinebl <katherine.briceno@kairosds.com>
2020-01-16 20:41:09 +0100

    Added json status to some api methods in badwords

--- routes/badwords.js
+++ routes/badwords.js
@@ -11,29 +11,24 @@ badwordRouter.get("/", async (req, res) => {
 
 badwordRouter.get("/:id", async (req, res) => {
     const badword = await repository.findOneWord(req.params.id);
-    res.status(200).json(badword);
+    (!badword) ? res.json({ status: "Badword Not Found" }) : res.json(badword);
 });
 
 badwordRouter.post("/", async (req, res) => {
     const badword = await repository.postOneWord(req.body);
-    console.log(badword);
-    res.status(200).json(badword);
+    res.json(badword);
 });
 
 badwordRouter.put("/:id", async (req, res) => {
     await repository.updateOneWord(req.params.id, req.body);
-    res.status(200);
-    res.json({
-        status: "Badword updated!"
-    });
+    // res.sendStatus(200);
+    res.json({ status: "Badword updated" });
 });
 
 badwordRouter.delete("/:id", async (req, res) => {
     await repository.deleteOneWord(req.params.id);
-    res.status(200);
-    res.json({
-        status: "Badword removed"
-    });
+    // res.sendStatus(200);
+    res.json({ status: "Badword removed" })
 });
 
 module.exports = badwordRouter;
