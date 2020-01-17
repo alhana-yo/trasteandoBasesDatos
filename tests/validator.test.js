d4bae49c88873dad75884b6805d30b330c3f1c77
katherinebl <katherine.briceno@kairosds.com>
2020-01-17 14:02:07 +0100

    Added validator test with jest

---
+++ tests/validator.test.js
@@ -0,0 +1,18 @@
+
+const validator = require('../validator.js');
+const badWords = require('../defBadWords.js');
+
+describe('comments validator', () => {
+    test('verify that the comment has forbidden words', () => {
+        const comment = 'hello bitch';
+        const input = validator.validator(comment, badWords)
+        expect(input.isIncluded).toBeTruthy();
+    });
+    test('verify that the comment does not contain prohibited words', () => {
+        const comment = 'hello people';
+        const input = validator.validator(comment, badWords)
+
+        expect(input.isIncluded).toBeFalsy();
+
+    })
+});
