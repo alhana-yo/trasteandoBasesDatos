ef6349617f783d5b08c7bc1621e8f6d119e12ed3
katherinebl <katherine.briceno@kairosds.com>
2020-01-19 18:58:51 +0100

    Added auth dependencies and certificates

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 const app = require("./controller");
-app.listen(3000, () => console.log("Server started in port 3000"));
+
+var fs = require('fs');
+var https = require('https');
+
+https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+}, app).listen(3443, () => {
+    console.log("Https server started in port 3443");
+});
+
+// app.listen(3000, () => console.log("Server started in port 3000"));
+
+
+
+
+
+
