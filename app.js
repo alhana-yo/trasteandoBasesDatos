3ae225fc71a4873eb4af9e807d97d89d7a3cf9f2
katherinebl <katherine.briceno@kairosds.com>
2020-01-20 14:41:02 +0100

    Everything works with new secure port

--- app.js
+++ app.js
@@ -44,14 +44,14 @@ var https = require('https');
 
 // users.init();
 
-// https.createServer({
-//     key: fs.readFileSync('server.key'),
-//     cert: fs.readFileSync('server.cert')
-// }, app).listen(3443, () => {
-//     console.log("Https server started in port 3443");
-// });
+https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+}, app).listen(3443, () => {
+    console.log("Https server started in port 3443");
+});
 
-app.listen(3000, () => console.log("Server started in port 3000"));
+// app.listen(3000, () => console.log("Server started in port 3000"));
 
 
 
