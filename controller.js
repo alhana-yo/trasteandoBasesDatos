97f6c6b14ea714f1236fcf1525f047a268269c32
Beatriz German <beatriz.german@kairosds.com>
2020-01-28 16:12:58 +0100

    Add permissions for put and delete methods from localhost: 8080

--- controller.js
+++ controller.js
@@ -13,6 +13,8 @@ app.use(function(req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+
   next();
 });
 // Routes
