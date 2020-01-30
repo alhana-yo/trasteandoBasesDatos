14254b0ff3e13360d5a615aa7ce52ca4f8ffe072
katherinebl <katherine.briceno@kairosds.com>
2020-01-30 15:53:11 +0100

    Again: protected PUT and DELETE endpoints, just admin can control them

--- routes/badwords.js
+++ routes/badwords.js
@@ -2,40 +2,35 @@ const express = require('express');
 const badwordRouter = express.Router();
 const passport = require("passport");
 
-const checkRole = require('../checkRole.js');
-
 const repository = require('../repositories/badwords.js');
+const checkRole = require('../utils/checkRole.js');
 
 badwordRouter.get("/", async (req, res) => {
     const badwords = await repository.findAllwords();
-    res.status(200).json(badwords);
+    res.json(badwords);
 });
 
 
 badwordRouter.get("/:id", async (req, res) => {
     const badword = await repository.findOneWord(req.params.id);
-    (!badword)
-        ? res.status(404).json({ msg: "Badword Not Found" })
-        : res.status(200).json(badword);
+    (!badword) ? res.json({ status: "Badword Not Found" }) : res.json(badword);
 });
 
-badwordRouter.post("/", passport.authenticate('jwt', { session: false }), async (req, res) => {
+badwordRouter.post("/", async (req, res) => {
     const badword = await repository.postOneWord(req.body);
-    res.status(200).json(badword);
+    res.json(badword);
 });
 
 badwordRouter.put("/:id", passport.authenticate('jwt', { session: false }), checkRole, async (req, res) => {
-    const badword = await repository.updateOneWord(req.params.id, req.body);
-    (!badword)
-        ? res.status(404).json({ msg: "Can't update: Badword Not Found" })
-        : res.status(200).json({ msg: "Badword updated" });
+    await repository.updateOneWord(req.params.id, req.body);
+    // res.sendStatus(200);
+    res.json({ status: "Badword updated" });
 });
 
 badwordRouter.delete("/:id", passport.authenticate('jwt', { session: false }), checkRole, async (req, res) => {
-    const badword = await repository.deleteOneWord(req.params.id);
-    (!badword)
-        ? res.status(404).json({ msg: "Can't remove: Badword Not Found" })
-        : res.status(200).json({ msg: "Badword removed" });
+    await repository.deleteOneWord(req.params.id);
+    // res.sendStatus(200);
+    res.json({ status: "Badword removed" })
 });
 
 module.exports = badwordRouter;
